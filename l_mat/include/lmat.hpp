5dafd63967c2f2fe15bb164517f7e96dca691516
lynnchan <512925430@qq.com>
2018-11-26 22:26:36 +0800

    Add multiply function

--- l_mat/include/lmat.hpp
+++ l_mat/include/lmat.hpp
@@ -27,20 +27,27 @@ public:
     typedef T value_type;
     typedef typename std::vector<T>::size_type l_size_type;
     typedef typename std::vector<std::vector<T> >::size_type l_v_size_type;
-    lMat();
+    lMat():_pd(nullptr){}
     lMat(lUint r,lUint c,T d)
     {
         std::vector<T> _b(c,d);
         LogInfo<<"type name "<<typeid(T).name();
         _pd=std::make_shared<std::vector<std::vector<T> > >(r,_b);
     }
-    l_v_size_type size() const {return _pd->size();}
+    l_v_size_type size() const{ return _pd==nullptr?0:_pd->size();}
     m_size size_m() const{return m_size(_pd->size(),_pd->at(0).size());}
 
     std::vector<T> & operator[](l_v_size_type idx) {return _pd->at(idx);}
-    lMat<double> operator*(lMat<T> & _m)
+    template<typename _T>  lMat<double> operator*(lMat<_T> & _m)
     {
         lMat<double> _mat(size_m()._c,size_m()._r,0);
+        for(int i=0;i<size_m()._c;i++)
+        {
+            for(int j=0;j<size_m()._r;j++)
+            {
+                _mat[i][j]=_pd->at(i)[j]*_m[i][j];
+            }
+        }
         return _mat;
     }
 
