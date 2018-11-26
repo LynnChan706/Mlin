0b2fb31472ebabcf3e2c98aed8a9d7abe04c2149
lynnchan <512925430@qq.com>
2018-11-26 09:53:04 +0800

    Create the l_mat library, a simple matrix multiplication library

---
+++ l_mat/include/l_mat.hpp
@@ -0,0 +1,58 @@
+﻿/*////////////////////////////////////////////////////////////////////////////
+ *A simple matrix named l_mat
+ *
+ *
+ *
+//lynn chan */
+
+#include <vector>
+#include <memory>
+#include <qdebug.h>
+#include <QDateTime>
+#define LogInfo  qDebug()<<"["<<(QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz")).toStdString().c_str() <<  "]" << __LINE__<<__FUNCTION__<<" : "
+
+typedef unsigned int l_uint;
+
+using namespace std;
+namespace lynn {
+
+struct m_size
+{
+    m_size(l_uint r,l_uint c) {_r=r;_c=c;}
+    l_uint _r;
+    l_uint _c;
+};
+
+
+template<typename T>class l_Mat
+{
+public:
+    typedef T value_type;
+    typedef typename std::vector<T>::size_type l_size_type;
+    typedef typename std::vector<std::vector<T>>::size_type l_v_size_type;
+    l_Mat();
+    l_Mat(l_uint r,l_uint c,T d)
+    {
+        std::vector<T> _b(c,d);
+        _pd=std::make_shared<std::vector<std::vector<T>>>(r,_b);
+    }
+    l_v_size_type size() const {return _pd->size();}
+    m_size size_m() const{return m_size(_pd->size(),_pd->at(0)->size());}
+
+    std::vector<T> & operator[](l_v_size_type idx) {return _pd->at(idx);}
+    l_Mat<double> operator*(l_Mat<T> & _m)
+    {
+        l_Mat<double> _mat(size_m()._c,size_m()._r,0);
+        return _mat;
+    }
+
+
+private:
+    std::shared_ptr<std::vector<std::vector<T>>> _pd;
+
+};
+
+}
+
+typedef lynn::l_Mat<int> l_Mati;
+typedef lynn::l_Mat<double> l_Matd;
---
+++ l_mat/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <QCoreApplication>
+#include "l_mat.hpp"
+using namespace lynn;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    LogInfo<<"hello";
+    lynn::l_Mat<int> li_mat(5,4,3.5);
+    lynn::l_Mat<int> li_mat1(5,4,2.5);
+    li_mat[0][0]=7;
+    l_Matd mat_2=li_mat*li_mat1;
+    LogInfo<<li_mat.size()<<" | "<<li_mat[0][0]<<" end";
+    return a.exec();
+}
